b2c1bb569dfe6583fca11feace884977b6c9f49f
jarlax <me@jarlax.com>
2013-06-04 23:29:36 +0300

    Implement image handler

--- public.py
+++ public.py
@@ -1,4 +1,6 @@
 import webapp2
+from model import Page
+from util import get_img_type
 
 
 class PublicHandler(webapp2.RequestHandler):
@@ -8,4 +10,10 @@ class PublicHandler(webapp2.RequestHandler):
 
 class ImageHandler(webapp2.RequestHandler):
     def get(self, key):
-        self.response.write('IMG GET ' + key)
+        page = Page.get_by_id(key)
+        if page and page.image:
+            type = get_img_type(page.name)
+            self.response.headers['Content-Type'] = str('image/' + type)
+            self.response.write(page.image)
+        else:
+            self.error(404)
--- util.py
+++ util.py
@@ -1,2 +1,11 @@
+import re
+
 name_pattern = '[_a-z0-9]+'  # Name pattern
 image_pattern = '%s\.(jpe?g|png)' % name_pattern # Image name pattern
+
+
+def get_img_type(name):
+    type = re.match(image_pattern, name).group(1)
+    if type == 'jpg':
+        type = 'jpeg'
+    return type
