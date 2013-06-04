dec15f2ec746fb3d1a8518103122c91fb1e2a92a
jarlax <me@jarlax.com>
2013-06-04 22:50:33 +0300

    Extract image name regex for further re-use

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from admin import AdminHandler
 from public import PublicHandler, ImageHandler
 import webapp2
 
-np = '[_a-z0-9]+'  # Name pattern
+name_pattern = '[_a-z0-9]+'  # Name pattern
+image_pattern = '%s\.(jpe?g|png)' % name_pattern # Image name pattern
 
 app = webapp2.WSGIApplication([
     webapp2.Route(r'/_s/pages', AdminHandler, handler_method='list',
                   methods=['GET']),
-    webapp2.Route(r'/_s/page/<key:%s>' % np, AdminHandler,
+    webapp2.Route(r'/_s/page/<key:%s>' % name_pattern, AdminHandler,
                   methods=['GET', 'PUT', 'DELETE']),
     webapp2.Route(r'/_s/page', AdminHandler, methods=['POST']),
     webapp2.Route(r'/_s/img', AdminHandler, handler_method='img',
                   methods=['POST']),
-    webapp2.Route(r'/<key:%s\.(jpe?g|png)>' % np, ImageHandler, methods=['GET']),
-    webapp2.Route(r'/<key:%s>' % np, PublicHandler, methods=['GET']),
+    webapp2.Route(r'/<key:%s>' % image_pattern, ImageHandler, methods=['GET']),
+    webapp2.Route(r'/<key:%s>' % name_pattern, PublicHandler, methods=['GET']),
     webapp2.Route(r'/', PublicHandler, methods=['GET'])
 ], debug=True)
 
