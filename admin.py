da8ecb7264fd3671475069c352316ad70d68144b
jarlax <me@jarlax.com>
2013-06-05 23:33:16 +0300

    Implement GET, POST, PUT, DELETE methods for /page resource; Enhanced test page to work with these methods

--- admin.py
+++ admin.py
@@ -4,16 +4,27 @@ from model import Page
 
 class AdminHandler(webapp2.RequestHandler):
     def get(self, key):
-        self.response.write('ADMIN GET ' + key)
+        page = Page.get_by_id(key)
+        if page:
+            self.response.write(page.toJson())
+        else:
+            self.error(404)
 
     def list(self):
         self.response.write('ADMIN LIST')
 
     def put(self, key):
-        self.response.write('ADMIN PUT ' + key)
+        page = Page.get_by_id(key)
+        json_str = self.request.body
+        page.mergeJson(json_str)
+        page.put()
 
     def post(self):
-        self.response.write('ADMIN POST')
+        page = Page(id='test') # TODO: fix issue with assigning ID
+        json_str = self.request.body
+        page.mergeJson(json_str)
+        page.put()
+        self.response.status = 201
 
     def img(self):
         req = self.request
@@ -25,4 +36,5 @@ class AdminHandler(webapp2.RequestHandler):
         self.response.status = 201
 
     def delete(self, key):
-        self.response.write('ADMIN DELETE ' + key)
+        page = Page.get_by_id(key)
+        page.key.delete()
--- admin/app.coffee
+++ admin/app.coffee
@@ -1,6 +1,14 @@
 Admin = ($scope, $http) ->
-  $scope.save = ->
+  $scope.key = 'test'
+  $scope.post = ->
     $http.post '_s/page', angular.toJson($scope.page)
+  $scope.put = ->
+    $http.put '_s/page/' + $scope.key, angular.toJson($scope.page)
+  $scope.delete = ->
+    $http.delete '_s/page/' + $scope.key
+  $scope.get = ->
+    $http.get('_s/page/' + $scope.key).success (resp) ->
+      $scope.page = resp
 
 # Angular bootstrap
 name = 'AdminPage'
