1ead8d1fde950c9f020b6858c55cf52db7c2eb25
jarlax <me@jarlax.com>
2013-06-05 22:53:27 +0300

    Fix minor style issues

--- util.py
+++ util.py
@@ -1,11 +1,11 @@
 import re
 
 name_pattern = '[_a-z0-9]+'  # Name pattern
-image_pattern = '%s\.(jpe?g|png)' % name_pattern # Image name pattern
+image_pattern = '%s\.(jpe?g|png)' % name_pattern  # Image name pattern
 
 
-def get_img_type(name):
-    type = re.match(image_pattern, name).group(1)
-    if type == 'jpg':
-        type = 'jpeg'
-    return type
+def get_img_type(img_name):
+    img_type = re.match(image_pattern, img_name).group(1)
+    if img_type == 'jpg':
+        img_type = 'jpeg'
+    return img_type
