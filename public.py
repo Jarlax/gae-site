fc8e64759d64fe60b53e026c43e628fe84fd0dc1
jarlax <me@jarlax.com>
2013-06-14 18:15:08 +0300

    Start adding Jinja2 support

--- public.py
+++ public.py
@@ -1,10 +1,19 @@
+import jinja2
 import webapp2
 from model import Page
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader('templates'),
+    extensions=['jinja2.ext.autoescape'])
 
 
 class PublicHandler(webapp2.RequestHandler):
     def get(self, page_id=''):
-        self.response.write('PUB GET ' + page_id)
+        values = {
+            'model': page_id
+        }
+        template = JINJA_ENVIRONMENT.get_template('page.html')
+        self.response.write(template.render(values))
 
     def get_file(self, file_id):
         page = Page.get_by_id(file_id)
